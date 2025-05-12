f000223160cf6a60b766166a691fa091aa9af2db
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-05-12 15:58:00 +0530

    updated

--- Level_2/Task_2/into.js
+++ Level_2/Task_2/into.js
@@ -1,20 +1,17 @@
-const blockct=document.querySelector(".sm-block")
-const hamburger=document.querySelector("#dash")
-console.log(blockct.style.display);
-function clickfn(){
-    if(blockct.style.display===''){
-        if(window.innerWidth<=600){
-            blockct.style.display='block';
-            hamburger.classList.remove("fa-bars")
-            hamburger.classList.add("fa-close")
+const blockct = document.querySelector(".sm-block");
+const hamburger = document.querySelector("#dash");
+function clickfn() {
+    const computedDisplay = window.getComputedStyle(blockct).display;
+    
+    if (computedDisplay === 'none' || computedDisplay === '') {
+        if (window.innerWidth <= 600) {
+            blockct.style.display = 'block';
+            hamburger.classList.remove("fa-bars");
+            hamburger.classList.add("fa-close");
         }
-        
-
-
-    }
-    else if(blockct.style.display==='block'){
-        blockct.style.display='';
-        hamburger.classList.remove("fa-close")
-        hamburger.classList.add("fa-bars")
+    } else {
+        blockct.style.display = 'none';
+        hamburger.classList.remove("fa-close");
+        hamburger.classList.add("fa-bars");
     }
 }
